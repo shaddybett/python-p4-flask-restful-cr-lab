602b2bb7d12fa1dcce3aed49fbc04b63e9b6f2ab
Shaddy <shadybett540@gmail.com>
2024-01-22 10:33:38 +0300

    migrated succesfully

---
+++ server/migrations/versions/67f5d67aea55_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 67f5d67aea55
+Revises: cda29e49ebc7
+Create Date: 2024-01-22 10:28:59.428750
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67f5d67aea55'
+down_revision = 'cda29e49ebc7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
